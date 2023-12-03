2c987133528c208ce0dca25957e273c7eebeefca
Akash Bhardwaj <akashbdj@gmail.com>
2023-12-03 18:31:35 +0530

    env specific base url

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -1,4 +1,7 @@
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL =
+  import.meta.env.MODE === 'production'
+    ? 'https://ramblings-backend.onrender.com'
+    : 'http://localhost:3000'
 
 export const API_ENDPOINTS = {
   POSTS: `${BASE_URL}/posts`,
